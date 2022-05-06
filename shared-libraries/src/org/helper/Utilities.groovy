c9966f5a96351e85a94d4ee2be8d48cd97c555e3
Godwin <gdwntheophilus25@gmail.com>
2022-05-06 09:50:25 +0530

    m

---
+++ shared-libraries/src/org/helper/Utilities.groovy
@@ -0,0 +1,9 @@
+package org.foo
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "echo -o ${args}"
+    steps.sh "ls -lrt /"
+  }
+}
