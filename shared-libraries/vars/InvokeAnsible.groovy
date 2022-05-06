eb43d9523c1ba8f82b3d0c25f295d90beea2dbff
Godwin <gdwntheophilus25@gmail.com>
2022-05-06 17:43:29 +0530

    comm

--- shared-libraries/vars/InvokeAnsible.groovy
+++ shared-libraries/vars/InvokeAnsible.groovy
@@ -1,20 +1,19 @@
 import org.helper.InvokeAnsibleHelper;
 
 def handleBodyInput(body,pipeline_params){
-    body.resolbeStrategy = Closure.DELEGATE_FIRST
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipeline_params
     body()
 }
 
 def call(body) {
+
+    def invokeAnsibleHelper = new InvokeAnsibleHelper(this)
     def pipeline_params = [:]
     // handleBodyInput(body,pipeline_params)
-    body.resolbeStrategy = Closure.DELEGATE_FIRST
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipeline_params
     body()
-    def invokeAnsibleHelper = new InvokeAnsibleHelper(this)
-    String credentialsId = pipeline_params.get('credentialsId')
-
     String credentialsId2 = pipeline_params.credentialsId
     println(credentialsId)
     println(credentialsId2)
