ee911ab20e6a6d7cba3c10405e91143d50fd75bc
Godwin <gdwntheophilus25@gmail.com>
2022-05-06 16:32:10 +0530

    comiit

---
+++ shared-libraries/src/org/helper/InvokeAnsibleHelper.groovy
@@ -0,0 +1,18 @@
+package org.helper
+
+class InvokeAnsibleHelper implements Serializable {
+    def steps
+    def InvokeAnsibleHelper(steps) {
+        this.steps = steps
+    }
+
+    String runAnsiblePlaybook(String playbookname, String hostsFileName) {
+
+        steps.rerty(3) {
+            steps.echo "running ansible playbook: ${playbookname}"
+            steps.echo "ansible-playbook -i " + hostsFileName + ' ' + playbookname
+        }
+        return "ansible-playbook -i " + hostsFileName + ' ' + playbookname
+    }
+    
+}
--- shared-libraries/vars/InvokeAnsible.groovy
+++ shared-libraries/vars/InvokeAnsible.groovy
@@ -1,6 +1,28 @@
 
+def handleBodyInput(body,parameters){
+    body.resolbeStrategy = Closure.DELEGATE_FIRST
+    body.delegate = parameters
+    body()
+}
 
 def call(body) {
     def parameters = [:]
-    echo "body: ${body}"
+    def invokeAnsibleHelper = new InvokeAnsibleHelper(this)
+    pipeline {
+        agent any 
+        options {
+            ansiColor('xterm')
+            timestamps()
+            skipDefaultCheckout true
+        }
+        stages {
+            stage('invokingAnsible') {
+                steps {
+                    script {
+                        invokeAnsibleHelper.runAnsiblePlaybook("playbooks/invoke-ansible.yml", "hosts/hosts")
+                    }
+                }
+            }
+        }
+    }
 }
