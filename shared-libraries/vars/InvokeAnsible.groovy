018009cb281cee5de4eefc6e6160705889752e54
Godwin <gdwntheophilus25@gmail.com>
2022-05-06 17:44:19 +0530

    c

--- shared-libraries/vars/InvokeAnsible.groovy
+++ shared-libraries/vars/InvokeAnsible.groovy
@@ -7,13 +7,14 @@ def handleBodyInput(body,pipeline_params){
 }
 
 def call(body) {
-
-    def invokeAnsibleHelper = new InvokeAnsibleHelper(this)
     def pipeline_params = [:]
     // handleBodyInput(body,pipeline_params)
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipeline_params
     body()
+    def invokeAnsibleHelper = new InvokeAnsibleHelper(this)
+    String credentialsId = pipeline_params.get('credentialsId')
+
     String credentialsId2 = pipeline_params.credentialsId
     println(credentialsId)
     println(credentialsId2)
